bf5ddeb1f27141c1d053cf9e281f4439fab864b8
Riya Saini <rsaini@opentext.com>
2025-03-13 22:31:14 +0530

    added episode 3

---
+++ Episode-3_Laying_the_foundation/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I'm h1 tag 🍃")
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
