343fc08bf58c51ebb3b831a720fcf0d5706f85d3
Riya Saini <rsaini@opentext.com>
2025-03-13 19:55:52 +0530

    added parcel

---
+++ Episode-2_Ignite-Our-App/App.js
@@ -0,0 +1,14 @@
+// const heading = React.createElement("h1", {}, "Hello Duniya from React!")
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I'm h1 tag")
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
