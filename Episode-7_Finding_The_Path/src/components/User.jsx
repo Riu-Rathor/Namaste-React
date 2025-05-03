7d27ba2c2d667984b3672a311bd4a59538439cc7
Riya Saini <rsaini@opentext.com>
2025-05-03 15:13:43 +0530

    study class based components

---
+++ Episode-7_Finding_The_Path/src/components/User.jsx
@@ -0,0 +1,11 @@
+const User = () => {
+    return (
+        <div className="user-card">
+            <h2>Name: Rishit Singh</h2>
+            <h3>Location: Saharanpur</h3>
+            <h4>Contact: 7817070558</h4>
+        </div>
+    );
+}
+
+export default User;
---
+++ Episode-7_Finding_The_Path/src/utils/restaurent_data.js
@@ -0,0 +1,95 @@
+const restaurents = [
+    {
+      "name": "Pizza Palace",
+      "image": "https://plus.unsplash.com/premium_photo-1673108852141-e8c3c22a4a22?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "rating": 4.5,
+      "deliveryTime": "30 mins",
+      "cuisine": "Italian",
+      "price": "₹250 for one",
+      "location": "MG Road, Bangalore"
+    },
+    {
+      "name": "Spicy Treat",
+      "image": "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D",
+      "rating": 3.6,
+      "deliveryTime": "25 mins",
+      "cuisine": "North Indian",
+      "price": "₹300 for one",
+      "location": "Koramangala, Bangalore"
+    },
+    {
+      "name": "Sushi Delight",
+      "image": "https://images.unsplash.com/photo-1504674900247-0877df9cc836?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D",
+      "rating": 4.7,
+      "deliveryTime": "40 mins",
+      "cuisine": "Japanese",
+      "price": "₹500 for one",
+      "location": "HSR Layout, Bangalore"
+    },
+    {
+      "name": "Burger House",
+      "image": "https://plus.unsplash.com/premium_photo-1670601440146-3b33dfcd7e17?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D",
+      "rating": 3.0,
+      "deliveryTime": "20 mins",
+      "cuisine": "Fast Food",
+      "price": "₹200 for one",
+      "location": "Indiranagar, Bangalore"
+    },
+    {
+      "name": "Tandoori Flames",
+      "image": "https://plus.unsplash.com/premium_photo-1663852297267-827c73e7529e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D",
+      "rating": 4.6,
+      "deliveryTime": "35 mins",
+      "cuisine": "Mughlai",
+      "price": "₹400 for one",
+      "location": "JP Nagar, Bangalore"
+    },
+    {
+      "name": "Green Leaf",
+      "image": "https://images.unsplash.com/photo-1606787366850-de6330128bfc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGZvb2R8ZW58MHx8MHx8fDA%3D",
+      "rating": 4.0,
+      "deliveryTime": "20 mins",
+      "cuisine": "Healthy",
+      "price": "₹350 for one",
+      "location": "Whitefield, Bangalore"
+    },
+    {
+      "name": "Taco Fiesta",
+      "image": "https://images.unsplash.com/photo-1493770348161-369560ae357d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGZvb2R8ZW58MHx8MHx8fDA%3D",
+      "rating": 3.8,
+      "deliveryTime": "30 mins",
+      "cuisine": "Mexican",
+      "price": "₹280 for one",
+      "location": "Bellandur, Bangalore"
+    },
+    {
+      "name": "Wok & Roll",
+      "image": "https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGZvb2R8ZW58MHx8MHx8fDA%3D",
+      "rating": 4.5,
+      "deliveryTime": "35 mins",
+      "cuisine": "Chinese",
+      "price": "₹320 for one",
+      "location": "Electronic City, Bangalore"
+    },
+    {
+      "name": "Sweet Cravings",
+      "image": "https://images.unsplash.com/photo-1414235077428-338989a2e8c0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fGZvb2R8ZW58MHx8MHx8fDA%3D",
+      "rating": 4.8,
+      "deliveryTime": "25 mins",
+      "cuisine": "Desserts",
+      "price": "₹180 for one",
+      "location": "Brigade Road, Bangalore"
+    },
+    {
+      "name": "Mediterranean Bites",
+      "image": "https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzZ8fGZvb2R8ZW58MHx8MHx8fDA%3D",
+      "rating": 4.6,
+      "deliveryTime": "40 mins",
+      "cuisine": "Mediterranean",
+      "price": "₹450 for one",
+      "location": "Indiranagar, Bangalore"
+    }
+  ]
+  
+
+  export default restaurents;
